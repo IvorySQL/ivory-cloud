895a73519f7bd7fb63d98d0d170290b432427d3e
shaolinchu <chushaolin@highgo.com>
2023-10-27 10:15:26 +0800

    add unit test for common and cloud native

---
+++ cloudnative/src/test/com/highgo/platform/apiserver/controller/K8sClusterControllerTest.java
@@ -0,0 +1,14 @@
+package com.highgo.cloud.auth.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AuthControllerTest {
+
+	@Test
+	public void testList() {
+		 Assert.assertEquals("1", "1");
+		//fail("Not yet implemented");
+	}
+
+}
