3d07997769de52d543c88bc3e92f87118d9eee3c
shaolinchu <chushaolin@highgo.com>
2023-10-30 16:06:43 +0800

    test failed test case for ut workflow

---
+++ cloudnative/src/test/java/com/highgo/platform/operator/service/impl/CrServiceImplTest.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.highgo.platform.operator.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CrServiceImplTest {
+
+    @Test
+    void testCreateCr() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testApplyCr() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testIsCrExist() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testPatchCrResource() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testPatchCrStorage() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testDeleteCr() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testDeleteAllPod() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRestartDatabase() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testNodeportSwitch() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testCreateBackup() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testApplyBackupPolicy() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testDeleteBackup() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testApplyConfigParam() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetInstanceVOFromCR() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetMasterPod() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testRestore() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testGetHgadminPort() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void testPatchCrUsers() {
+        fail("Not yet implemented");
+    }
+
+}
