99e048a4e8d24914ab337b600db7ce1428ce6a65
shaolinchu <chushaolin@highgo.com>
2023-11-02 17:55:15 +0800

    add junit test case for testListByFilter

--- cloudnative/src/test/java/com/highgo/platform/apiserver/service/impl/InstanceServiceImplTest.java
+++ cloudnative/src/test/java/com/highgo/platform/apiserver/service/impl/InstanceServiceImplTest.java
@@ -35,13 +35,20 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 import com.highgo.cloud.enums.BackupStatus;
 import com.highgo.cloud.enums.InstanceStatus;
 import com.highgo.platform.apiserver.model.dto.InstanceDTO;
 import com.highgo.platform.apiserver.model.po.BackupPO;
 import com.highgo.platform.apiserver.model.po.ExtraMetaPO;
+import com.highgo.platform.apiserver.model.po.InstanceNetworkPO;
 import com.highgo.platform.apiserver.model.po.InstancePO;
+import com.highgo.platform.apiserver.model.po.K8sClusterInfoPO;
 import com.highgo.platform.apiserver.model.vo.request.CreateInstanceVO;
 import com.highgo.platform.apiserver.model.vo.response.ActionResponse;
 import com.highgo.platform.apiserver.model.vo.response.InstanceVO;
@@ -141,7 +148,6 @@ class InstanceServiceImplTest {
         // namespace, instanceName);
 
         InstanceException exception = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
 
@@ -161,7 +167,6 @@ class InstanceServiceImplTest {
         // 实例不存在或已删除
         createInstanceVO.setOriginalInstanceId("originalInstanceId");
         InstanceException exception_instance_not_exist = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
 
@@ -170,7 +175,6 @@ class InstanceServiceImplTest {
         // 实例已删除
         when(instanceRepository.findById(any(String.class))).thenReturn(getDeletedInstances());
         InstanceException exception_instance_is_deleted = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(InstanceError.INSTANCE_NOT_EXIST.message(), exception_instance_is_deleted.getMessage());
@@ -178,7 +182,6 @@ class InstanceServiceImplTest {
         // 实例存在,但是当前状态不允许执行恢复
         when(instanceRepository.findById(any(String.class))).thenReturn(getNotDeleted_creating_Instances());
         InstanceException exception_instance_nofile_not_allow_restore = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
 
@@ -188,7 +191,6 @@ class InstanceServiceImplTest {
         // 实例存在,但是备份不存在
         when(instanceRepository.findById(any(String.class))).thenReturn(getRunning_Instances());
         BackupException exception_instance_backupfile_not_exist = assertThrows(BackupException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(BackupError.BACKUP_NOT_EXIST.message(), exception_instance_backupfile_not_exist.getMessage());
@@ -198,7 +200,6 @@ class InstanceServiceImplTest {
         Optional<BackupPO> optionalBackupPO = getBackupFiles_notAllowRestoreOptional();
         when(backupRepository.findById(any(String.class))).thenReturn(optionalBackupPO);
         BackupException exception_instance_not_allow_restore = assertThrows(BackupException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(BackupError.BACKUP_NOT_ALLOW_OPERATE.message(), exception_instance_not_allow_restore.getMessage());
@@ -209,7 +210,6 @@ class InstanceServiceImplTest {
         optionalBackupPO.get().setStatus(BackupStatus.COMPLETED);
         when(instanceRepository.findById(any(String.class))).thenReturn(getCpuLargerThanNewInstance());
         InstanceException exception_cpu = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(InstanceError.INSTANCE_NOT_ALLOW_DEMOTE.message(), exception_cpu.getMessage());
@@ -221,7 +221,6 @@ class InstanceServiceImplTest {
         optionalBackupPO.get().setStatus(BackupStatus.COMPLETED);
         when(instanceRepository.findById(any(String.class))).thenReturn(getMemoryLargerThanNewInstance());
         InstanceException exception_memory = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(InstanceError.INSTANCE_NOT_ALLOW_DEMOTE.message(), exception_memory.getMessage());
@@ -234,7 +233,6 @@ class InstanceServiceImplTest {
         optionalBackupPO.get().setStatus(BackupStatus.COMPLETED);
         when(instanceRepository.findById(any(String.class))).thenReturn(getStorageLargerThanNewInstance());
         InstanceException exception_storage = assertThrows(InstanceException.class, () -> {
-            // 在这里调用该方法
             instanceServiceImpl.createInstance(createInstanceVO);
         });
         assertEquals(InstanceError.INSTANCE_NOT_ALLOW_DEMOTE.message(), exception_storage.getMessage());
@@ -392,23 +390,125 @@ class InstanceServiceImplTest {
     }
 
     @Test
-    void testDeleteInstanceCallback() {
-
-    }
-
-    @Test
     void testGetVO() {
+        String id = "123456";
+        when(instanceRepository.findById(id)).thenReturn(getWithClusterId_Instances());
 
+        List<InstanceNetworkPO> instanceNetworkPOs = getInstanceNetworkPOById();
+        when(instanceNetworkRepository.listByInstanceId(id)).thenReturn(instanceNetworkPOs);
+
+        K8sClusterInfoPO k8sClusterInfoPO = new K8sClusterInfoPO();
+        k8sClusterInfoPO.setClusterName("clusterName");
+        when(k8sClusterService.getInfoByClusterId(any(String.class))).thenReturn(k8sClusterInfoPO);
+        InstanceVO instanceVO = instanceServiceImpl.getVO(id);
+
+        assertEquals(instanceVO.getClusterName(), k8sClusterInfoPO.getClusterName());
+        assertEquals(instanceVO.getNetwork().size(), instanceNetworkPOs.size());
+        assertEquals(instanceVO.getNetwork().get(0).getNodePort(), instanceNetworkPOs.get(0).getNodePort());
     }
 
+    /**
+     * 获取允许被删除的实例
+     * @return
+     */
+    private Optional<InstancePO> getWithClusterId_Instances() {
+        InstancePO instancePO = new InstancePO();
+        instancePO.setIsDeleted(false);
+        instancePO.setStatus(InstanceStatus.RUNNING);
+        instancePO.setClusterId("clusterId");
+        Optional<InstancePO> optionalInstancePo = Optional.of(instancePO);
+
+        return optionalInstancePo;
+    }
+
+    /**
+     * get instance network
+     * @return
+     */
+    public List<InstanceNetworkPO> getInstanceNetworkPOById() {
+        InstanceNetworkPO instanceNetworkPO = new InstanceNetworkPO();
+        instanceNetworkPO.setId("1");
+        instanceNetworkPO.setNodePort(6060);
+        List<InstanceNetworkPO> instanceNetworkPOs = new ArrayList<InstanceNetworkPO>();
+        instanceNetworkPOs.add(instanceNetworkPO);
+
+        return instanceNetworkPOs;
+    }
     @Test
     void testGetDTO() {
+        String instanceId = "123456";
+        // 测试实例不存在
+        Optional<InstancePO> instancePo_empty = Optional.empty();
+
+        when(instanceRepository.findById(instanceId)).thenReturn(instancePo_empty);
+        InstanceException exception = assertThrows(InstanceException.class, () -> {
+
+            instanceServiceImpl.getDTO(instanceId);
+        });
+        assertEquals(InstanceError.INSTANCE_NOT_EXIST.message(), exception.getMessage());
+
+        // 测试 获取产品的属性
+        when(instanceRepository.findById(instanceId)).thenReturn(getWithClusterId_Instances());
+        when(extraMetaService.findAllByInstanceId(instanceId)).thenReturn(findExtrameta());
+        InstanceDTO instanceDTO = instanceServiceImpl.getDTO(instanceId);
+        assertEquals("value1", instanceDTO.getExtraMeta().get("name1"));
 
     }
 
     @Test
     void testListByFilter() {
+        // List<InstanceNetworkPO> instanceNetworkPOList = instanceNetworkRepository.list();
+        when(instanceNetworkRepository.list()).thenReturn(getInstanceNetworkPOList());
 
+        // Page<InstancePO> page = instanceRepository.findAll(specification, pageable);
+        Specification<InstancePO> spec = any(Specification.class);
+        PageRequest pageable = any(PageRequest.class);
+
+        // 设置mock行为
+        // Page<InstancePO> expectedPage = mock(Page.class);
+        when(instanceRepository.findAll(spec, pageable)).thenReturn(getInstancePOByPage());
+
+        instanceServiceImpl.listByFilter(1, "filter", "clusterId", 10, 20);
+    }
+
+    private List<InstanceNetworkPO> getInstanceNetworkPOList() {
+        InstanceNetworkPO instanceNetworkPO1 = new InstanceNetworkPO();
+        instanceNetworkPO1.setInstanceId("0001");
+        instanceNetworkPO1.setNodePort(6060);
+
+        InstanceNetworkPO instanceNetworkPO2 = new InstanceNetworkPO();
+        instanceNetworkPO2.setInstanceId("0002");
+        instanceNetworkPO2.setNodePort(6061);
+
+        List<InstanceNetworkPO> instanceNetworkPOList = new ArrayList<InstanceNetworkPO>();
+        instanceNetworkPOList.add(instanceNetworkPO1);
+        instanceNetworkPOList.add(instanceNetworkPO2);
+
+        return instanceNetworkPOList;
+    }
+
+    private Page<InstancePO> getInstancePOByPage() {
+        // 创建PageRequest对象，指定页码、每页大小和排序规则（可选）
+        PageRequest pageRequest = PageRequest.of(10, 20, Sort.by("name"));
+
+        InstancePO instancePO1 = new InstancePO();
+        instancePO1.setId("0001");
+        instancePO1.setClusterId("0001");
+        instancePO1.setName("name1");
+
+        InstancePO instancePO2 = new InstancePO();
+        instancePO1.setId("0002");
+        instancePO2.setClusterId("0002");
+        instancePO2.setName("name2");
+
+        List<InstancePO> instanceList = new ArrayList<InstancePO>();
+        instanceList.add(instancePO1);
+        instanceList.add(instancePO2);
+
+        long totalElements = 100;
+        Page<InstancePO> page = new PageImpl<>(instanceList, pageRequest, totalElements);
+
+        return page;
     }
 
     @Test
